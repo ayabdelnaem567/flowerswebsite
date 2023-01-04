99809276bc5b37993276a02819fd36b3f05b165f
ayabdelnaem567 <121629898+ayabdelnaem567@users.noreply.github.com>
2023-01-04 20:42:47 +0200

    Add files via upload

---
+++ flower-store-website/flower store website/about.php
@@ -0,0 +1,188 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom admin css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php @include 'header.php'; ?>
+
+<section class="heading">
+    <h3>about us</h3>
+    <p> <a href="home.php">home</a> / about </p>
+</section>
+
+<section class="about">
+
+    <div class="flex">
+
+        <div class="image">
+            <img src="images/about-img-1.png" alt="">
+        </div>
+
+        <div class="content">
+            <h3>why choose us?</h3>
+            <p>all about our ultimate love for flowers and exceeding our clients expectations!</p>
+            <a href="shop.php" class="btn">shop now</a>
+        </div>
+
+    </div>
+
+    <div class="flex">
+
+        <div class="content">
+            <h3>what we provide?</h3>
+            <p>The main objective of our shop is to fullfill all customers needs and give them better products.</p>
+            <a href="contact.php" class="btn">contact us</a>
+        </div>
+
+        <div class="image">
+            <img src="images/about-img-2.jpg" alt="">
+        </div>
+
+    </div>
+
+    <div class="flex">
+
+        <div class="image">
+            <img src="images/about-img-3.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>who we are?</h3>
+            <p>Our flower shop's purpose is to provide our customers with quality flowers and floral arrangements at a fair price.</p>
+            <a href="#reviews" class="btn">clients reviews</a>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="reviews" id="reviews">
+
+    <h1 class="title">client's reviews</h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/pic-1.png" alt="">
+            <p>The Packaging is supreme and the flowers were fresh</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>Mohamed Abbasyyy</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-2.png" alt="">
+            <p>My bouquet smelled amazing , will be ordering again soon.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>Samiha el tahrir</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-3.png" alt="">
+            <p>The Flowers came in very fresh , my wife loved them.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>john biden</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-4.png" alt="">
+            <p>Great website, came in handy for last minute ordering</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>karmen el dab3</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-5.png" alt="">
+            <p>Very High Quality and great delivery time.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>Adel shakalolo</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-6.png" alt="">
+            <p>recived them as a gift, they looked organic and had a fabouls scent.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3> Lee el sharkawy</h3>
+        </div>
+
+    </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<?php @include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
